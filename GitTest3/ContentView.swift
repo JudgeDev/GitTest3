9edb26266682cd0021e3809d45981a7890a75cc9
JudgeDev <judge.py@gmail.con>
2025-06-24 15:43:19 +0200

    Add test text

--- GitTest3/ContentView.swift
+++ GitTest3/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Text Change")
+            Text("Test2 Text")
         }
         .padding()
     }
